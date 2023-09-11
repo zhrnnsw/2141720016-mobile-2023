11b343d21242c73f9f11267c2fe66eaf96a0a7d7
Zahra Annisa Wahono <zahraannisawahono08@gmail.com>
2023-09-11 10:43:16 +0700

    Update Null Safety & Late Variable

--- week-2/src/29_2141720016_nullsafetylatevar.dart
+++ week-2/src/29_2141720016_nullsafetylatevar.dart
@@ -1,18 +1,20 @@
 
  void main() {
   int? nilai; // Variabel yang bisa berisi null
-  print(nilai); // Output: null
+  //print(nilai); // Output: null
+  
+  
   //print(nilai + 1);
   
-  nilai = 5; // Variabel diinisialisasi dengan nilai integer
-  print(nilai); // Output: 5
-  print(nilai + 1);
+
  
 
 
   late int nilai_late; // Variabel late, belum diinisialisasi
+  //print(nilai_late); // Output: Error karena belum diinisialisasi
+
   //print(nilai_late); // Output: null (default saat belum diinisialisasi)
   
-  nilai_late = 5; // Variabel diinisialisasi dengan nilai integer
-  print(nilai_late); // Output: 5
+  //nilai_late = 5; // Variabel diinisialisasi dengan nilai integer
+  //print(nilai_late); // Output: 5
 }
