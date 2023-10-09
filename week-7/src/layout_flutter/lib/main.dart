512df174b91dce4f0f16f363a199cfcbe7e3c413
Zahra Annisa Wahono <zahraannisawahono08@gmail.com>
2023-10-09 09:39:33 +0700

    Praktikum 1

--- week-7/src/layout_flutter/lib/main.dart
+++ week-7/src/layout_flutter/lib/main.dart
@@ -3,18 +3,57 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          Expanded(
+            /* soal 1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /* soal 2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+          /* soal 3*/
+          Icon(
+            Icons.star,
+            color: const Color.fromARGB(255, 183, 60, 51),
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout: Zahra Annisa Wahono 2141720016',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Column(
+          children: [
+            titleSection,
+            // Tambahkan widget lain di sini sesuai kebutuhan Anda
+          ],
         ),
       ),
     );
