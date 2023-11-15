290882f8c173f52914122a1416bcda8484f95984
Zahra Annisa Wahono <zahraannisawahono08@gmail.com>
2023-11-16 06:56:46 +0700

    W12: Soal 3

--- week-12/README.md
+++ week-12/README.md
@@ -9,24 +9,11 @@
 **Soal 1**
 Tambahkan nama panggilan Anda pada title app sebagai identitas hasil pekerjaan Anda.
 ```
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Zahra App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+return Scaffold(
+      appBar: AppBar(
+        title: const Text('Back from the Future - Zahra'),
       ),
-      home: const FuturePage(),
-    );
-  }
-}
 ```
-
-<img src="img/praktikum1.gif">
 
 **Soal 2** Carilah judul buku favorit Anda di Google Books, lalu ganti ID buku pada variabel `path`
 ```
@@ -38,3 +25,26 @@ class MyApp extends StatelessWidget {
   }
   ```
 <img src="img/image-1.png">
+
+**Soal 3** Jelaskan maksud kode langkah 5 tersebut terkait substring dan catchError!
+```
+            onPressed: () {
+                setState(() {});
+                getData()
+                .then((value){
+                  result = value.body.toString().substring(0,450);
+                  setState(() {
+                    
+                  });
+                }).catchError((_){
+                  result = 'An error has occured';
+                  setState(() {
+                    
+                  });
+                });
+              },
+```
+
+Pada langkah kelima, dilakukan penggunaan metode substring(0, 450) untuk mengekstraksi 450 karakter pertama dari respons HTTP untuk membatasi panjang teks yang akan ditampilkan dalam aplikasi. Penggunaan catchErrordigunakan untuk mengatasi kemungkinan terjadinya kesalahan selama proses permintaan data. Jika terdapat kesalahan saat pengambilan data, maka  pesan 'An error has occurred' akan dimasukkan ke variabel result dan ditampilkan.
+
+<img src="img/praktikum1.gif">
--- week-12/src/books/lib/main.dart
+++ week-12/src/books/lib/main.dart
@@ -32,28 +32,53 @@ class FuturePage extends StatefulWidget {
 
 class _FuturePageState extends State<FuturePage> {
   String result = '';
+  bool isLoading = false;
 
-  Future<Response> getData() async{
+  Future<Response> getData() async {
     const authority = 'www.googleapis.com';
     const path = '/books/v1/volumes/9-COCwAAQBAJ';
     Uri url = Uri.https(authority, path);
     return http.get(url);
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Back from the Future'),
+        title: const Text('Back from the Future - Zahra'),
       ),
       body: Center(
         child: Column(
           children: [
             const Spacer(),
-            ElevatedButton(onPressed: () {}, child: const Text('GO!')),
+            ElevatedButton(
+              style: isLoading
+                  ? ElevatedButton.styleFrom(primary: Colors.grey)
+                  : null,
+              child: const Text('GO!'),
+              onPressed: isLoading
+                  ? null
+                  : () {
+                      setState(() {
+                        isLoading = true;
+                      });
+                      getData().then(
+                        (value) {
+                          result = value.body.toString().substring(0, 450);
+                        },
+                      ).catchError((_) {
+                        result = 'An error has occurred';
+                      }).whenComplete(() {
+                        setState(() {
+                          isLoading = false;
+                        });
+                      });
+                    },
+            ),
             const Spacer(),
             Text(result),
             const Spacer(),
-            const CircularProgressIndicator(),
+            if (isLoading) const CircularProgressIndicator(),
             const Spacer(),
           ],
         ),
